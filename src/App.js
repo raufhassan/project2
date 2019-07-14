e7548d4db823ef8a01f22a7c2187479665739f2e
raufhassan <raufhassan@gmail.com>
2019-07-14 20:40:47 +0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Navbar, NavbarBrand} from 'reactstrap'
+import { directive } from '@babel/types';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Navbar dark color="dark" className="navbar-expand-sm fixed-top">
+      <div className="container">
+        <NavbarBrand href="/">Tesla Engineering</NavbarBrand>
+       
+        <button
+          className="navbar-toggler "
+          type="button "
+          data-toggle="collapse"
+          data-target="#Nav1"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse " id="Nav1">
+            <ul className="nav navbar-nav ml-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Clients</a>
+                </li>
+            </ul>
+           
+            </div>
+      </div>
+    </Navbar>
+  </div>
+    
   );
 }
 
